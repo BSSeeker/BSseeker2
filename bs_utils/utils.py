c0194a4a7e2d6c51e74b065c4021efc58ca87e8f
guoweilong <guoweilong@gmail.com>
2015-12-29 21:08:47 +0800

    v2.0.10

--- bs_utils/utils.py
+++ bs_utils/utils.py
@@ -23,7 +23,7 @@ def reverse_compl_seq(strseq):
 
 def show_version() :
     print ""
-    print "     BS-Seeker2 v2.0.8 - Sept 5, 2014     "
+    print "     BS-Seeker2 v2.0.10 - Nov 5, 2015"
     print ""
 
 
